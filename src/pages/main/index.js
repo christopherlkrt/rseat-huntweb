f94898d017c1716ff2d081642cf32ae6455878e6
Christopher <christopherkrt1@gmail.com>
2019-12-15 23:27:20 -0300

    first commit, initial setup & structure

---
+++ src/pages/main/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import api from '../../services/api';
+
+import './styles.css';
+
+export default class Main extends Component {
+    state = {
+        users:[],
+    }
+    componentDidMount() {
+        this.loadUsers();
+    }
+
+    loadUsers = async () => {
+        const response = await api.get('users');
+        this.setState({users: response.data});
+    };
+
+    render() {
+        const { users } = this.state;
+    return (
+        <div className="user-list">
+        {users.map(user => (
+            <article key={user.id}>
+                <strong>{user.name}</strong>
+                <p>{user.email}</p>
+                <p>{user.job}</p>
+            </article>
+        ))}
+    </div>
+    );
+    }
+}
