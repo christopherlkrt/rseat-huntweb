b24940ec4f9b8b5aac84c45745ba4eb7ff6f7d88
Christopher <christopherkrt1@gmail.com>
2019-12-17 00:14:17 -0300

    feat: id passado e busca informacoes do usuario para listagem

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
-
+import api from '../../services/api';
 export default class User extends Component {
+
+  state = {
+    user:{},
+  };
+
+  async componentDidMount(){
+    const { id } = this.props.match.params;
+
+    const response = await api.get(`/users/${id}`);
+
+    this.setState({ user: response.data });
+  }
+
   render() {
-    return <h1>User</h1>;
+    const { user } = this.state;
+
+    return (
+      <div className="user-info">
+        <h1>{user.name}</h1>
+        <p>{user.email}</p>
+        <p>{user.job}</p>
+        <p>{user.birth}</p>
+      </div>
+    );
   }
 }
